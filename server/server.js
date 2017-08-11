8f163b284eebaae5ee0c20ed137d4721b0f4d928
徐绍平 <455255849@qq.com>
2017-08-11 16:51:10 +0800

    ww

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ var multer  = require('multer');
 var storage = multer.diskStorage({
   //设置上传后文件路径，uploads文件夹会自动创建。
   destination: function (req, file, cb) {
-    cb(null, './public/upload')
+    cb(null, './public/uploads')
   },
   //给上传文件重命名，获取添加后缀名
   filename: function (req, file, cb) {
