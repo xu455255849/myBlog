e87204420706bdfa0115a5dcb205a515225abd15
徐绍平 <455255849@qq.com>
2017-08-11 17:58:34 +0800

    w

--- src/pages/publish.vue
+++ src/pages/publish.vue
@@ -34,9 +34,8 @@
         <Upload
           ref="upload"
           type="drag"
-          action="//106.14.205.222:8080/image/upload"
+          action="//106.14.205.222:8080/upload"
           :format="['jpg','jpeg','png']"
-          :headers="heads"
           :on-success="handleSuccess"
           :max-size="2048"
           :on-format-error="handleFormatError"
@@ -77,9 +76,6 @@
     name: 'page-publish',
     data() {
       return {
-        heads: {
-            "Content-Security-Policy": "img-src *"
-        },
         preview: false,
         title: '',
         intro: '',
