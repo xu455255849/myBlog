2385c06fce58f07441dc3ac87df7514cea3b33d3
xushaoping <455255849@qq.com>
2018-02-08 14:37:18 +0800

    reset样式

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ html, body {
 }
 
 /* 重置文本格式元素 */
-a { text-decoration: none; }
+a { text-decoration: none; color: #333 }
 
 /* 清除内外边距 */
 body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, /* structural elements 结构元素 */
@@ -33,7 +33,6 @@ dl, dt, dd, ul, ol, li, /* list elements 列表元素 */
 pre, /* text formatting elements 文本格式元素 */
 fieldset, lengend, button, input, textarea, /* form elements 表单元素 */
 th, td { /* table elements 表格元素 */
-  margin: 0;
   padding: 0;
 }
 
@@ -53,5 +52,10 @@ table {
 
 /* 重置列表元素 */
 ul, ol { list-style: none; }
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
 
 </style>
